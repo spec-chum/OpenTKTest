c85f3428f10355ff713945d3eaf74f66d5112eaa
Spec-Chum <specchumster@gmail.com>
2015-02-13 23:01:35 +0000

    Initial commit

---
+++ OpenTKtest/Source/Program.cs
@@ -0,0 +1,108 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.IO;
+
+namespace OpenTKTest
+{
+    class Game : GameWindow
+    {
+        int vao, vbo, program;
+
+        float[] verts =
+        {
+            0.0f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f
+        };
+
+        string loadShaderFromFile(string file)
+        {
+            using(StreamReader sr = new StreamReader(file))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public Game() : base(800, 600, GraphicsMode.Default, "OpenTK")
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color4.CornflowerBlue);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.GenBuffers(1, out vbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(9 * sizeof(float)), verts, BufferUsageHint.StaticDraw);
+
+            GL.GenVertexArrays(1, out vao);
+            GL.BindVertexArray(vao);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, IntPtr.Zero);
+
+            string shaderData = loadShaderFromFile("./Shaders/vertex.vert");
+            int vs = GL.CreateShader(ShaderType.VertexShader);            
+            GL.ShaderSource(vs, shaderData);
+            GL.CompileShader(vs);
+
+            shaderData = loadShaderFromFile("./Shaders/fragment.frag");
+            int fs = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fs, shaderData);
+            GL.CompileShader(fs);
+
+            program = GL.CreateProgram();
+            GL.AttachShader(program, vs);
+            GL.AttachShader(program, fs);
+            GL.LinkProgram(program);
+
+            GL.DetachShader(program, vs);
+            GL.DetachShader(program, fs);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y,
+                        ClientRectangle.Width, ClientRectangle.Height);            
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Keyboard[Key.Escape])
+                Exit();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.UseProgram(program);
+
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            using (Game game = new Game())
+            {
+                game.Run(30.0);
+            }
+        }
+    }
+}
